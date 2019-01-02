439ad98cb4656f7ecfb7675aca216393041206ba
Hieu C. Nguyen <nc.hieu@live.com>
2019-01-02 11:32:34 +1100

    add API layout, update Docs layout

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,51 +2,6 @@
 // Copyright 2018 - Vasern.com
 
 /**
- * Method use to create HTML element
- * @param {string} tagName 
- * @param { children: Array<HTMLElement> | HTMLElement, onClick: Function, className: string, styles.string} options 
- */
-function createElement(tagName, options) {
-    var element = document.createElement(tagName);
-
-    if (options) {
-        let { children, onClick, className, styles, innerText, ...rest } = options;
-        if (children) {
-            
-            if (Array.isArray(children)) {
-                children.forEach(item => element.appendChild(item));
-            } else {
-                element.appendChild(children);
-            }
-        }
-
-        if (className) {
-            element.className = className;
-        }
-
-        if (onClick) {
-            element.onclick = onClick;
-        }
-
-        if (styles) {
-            element.setAttribute('style', styles);
-        }
-
-        if (innerText) {
-            element.innerText = innerText;
-        }
-
-        if (rest) {
-            for (let prop in rest) {
-                element.setAttribute(prop, rest[prop]);
-            }
-        }
-    }
-
-    return element;
-}
-
-/**
  * Method use to control state of 'next' and 'previous' buttons
  * @param {*} index: current index
  * @param {*} max: max index allowed
